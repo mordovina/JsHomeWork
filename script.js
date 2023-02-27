f2217b50b1b7716e48fc569bd7d67f39bbaa45d2
yuliya mordovina <mordovina_y@list.ru>
2023-02-27 12:52:22 +0300

    homework 1

---
+++ script.js
@@ -0,0 +1,8 @@
+/*const degreesCelsius = +prompt ("Введите температуру в градусах Цельсия");
+const degreesFahrenheit = (9 / 5) * degreesCelsius + 32;
+alert (`Цельсий: ${degreesCelsius}, Фаренгейт: ${degreesFahrenheit.toFixed(1)}`);*/
+
+
+/*const name = "Юлия";
+const admin = name;
+console.log(admin);*/
