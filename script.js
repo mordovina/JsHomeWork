070efc8f70a70b0f6a151d8d69dbb2e1cfcbfbe3
yuliya mordovina <mordovina_y@list.ru>
2023-03-12 12:42:58 +0300

    homework 5

--- script.js
+++ script.js
@@ -150,6 +150,7 @@ console.log (`Деление: ${division(number1,number2)}`);*/
 /*ДЗ 4
 
 Задание 1
+
 Необходимо с помощью цикла for вывести следующие 11 строк в консоль:
 0 – это ноль
 1 – нечетное число
@@ -170,6 +171,7 @@ for (let i = 0; i <= 10; i++) {
 }
 
 Задание 2
+
 Дан массив [1, 2, 3, 4, 5, 6, 7]
 Сделайте из этого массива следующий [1, 2, 3, 6, 7]
 
@@ -178,6 +180,7 @@ arr.splice(3,2);
 console.log(arr);
 
 Задание 3
+
 Используя Math.random() вам необходимо генерировать цифры от 0 до 9, и создать массив состоящий из 5 
 таких элементов
 1. Рассчитать сумму элементов этого массива
@@ -203,4 +206,132 @@ for (let i = 0; i < randomArray.length; i++) {
         findElement = true;
     }    
 }
-console.log ((findElement === true) ? "В массиве есть число  3" : "В массиве нет числа  3");*/
+console.log ((findElement === true) ? "В массиве есть число  3" : "В массиве нет числа  3");
+
+ДЗ 5
+ 
+1. Дан объект numbers. Необходимо в консоль вывести все значения больше или равные 3.
+
+const numbers = {
+  keyin1: 1,
+  keyin2: 2,
+  keyin3: 3,
+  keyin4: 4,
+  keyin5: 5,
+  keyin6: 6,
+  keyin7: 7,
+}
+  
+for (const key in numbers) {
+  if (numbers [key] >= 3) {
+  console.log(numbers[key]);
+  }
+}
+  
+2. Необходимо из объекта, который лежит в константе post вывести значения, к которым приписан комментарий,
+в консоль.
+  
+const post = {
+  author: "John", // вывести этот текст
+  postId: 23,
+  comments: [
+  {
+  userId: 10,
+  userName: "Alex",
+  text: "lorem ipsum",
+  rating: {
+   likes: 10,
+   dislikes: 2, // вывести это число
+  },
+ },
+{
+  userId: 5, // вывести это число
+  userName: "Jane",
+  text: "lorem ipsum 2", // вывести этот текст
+  rating: {
+    likes: 3,
+    dislikes: 1,
+    },
+  },
+ ],
+};
+  
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
+  
+3. Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя метод forEach.
+  
+const products = [
+{
+id: 3,
+price: 200,
+},
+{
+id: 4,
+price: 900,
+},
+{
+id: 1,
+price: 1000,
+},
+];
+
+products.forEach(product => {
+  product.price *= 0.85;
+});
+
+console.log(products);
+  
+4. Необходимо вывести в консоль массив продуктов в котором есть хоть одна фотография используя метод 
+filter. Исходные данные - массив products.
+Необходимо отсортировать массив products используя метод sort по цене, начиная с самой маленькой, 
+заканчивая самой большой ценой, после чего вывести отсортированный массив в консоль.
+  
+const products2 = [
+{
+   id: 3,
+   price: 127,
+  photos: [
+    "1.jpg",
+    "2.jpg",
+  ],
+},
+{
+  id: 5,
+  price: 499,
+  photos: [],
+},
+{
+  id: 10,
+  price: 26,
+  photos: [
+    "3.jpg",
+  ],
+},
+{
+  id: 8,
+  price: 78,
+},
+];
+  
+const productsPhoto = products2.filter(product => product.photos >='.jpg');
+console.log(productsPhoto);
+products2.sort((min,max) => min.price - max.price);
+console.log(products2);
+  
+5. Дано 2 массива.  Вам необходимо объединить 2 этих массива, чтобы значения первого массива были 
+ключами, а значения второго массива — значениями.
+  
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+  
+const week = {};
+  
+for (let i = 0; i < en.length; i++) {
+    if (en.length === ru.length) {
+        week [en[i]] = ru[i];
+    }
+}
+console.log(week);*/
